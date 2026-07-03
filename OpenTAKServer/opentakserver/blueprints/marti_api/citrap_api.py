8d593f3ca14ca53f3142ababf6238d356e338266
Audrius Palisaitis <audrius.palisaitis@asseco.lt>
2026-07-03 13:45:41 +0300

    lzd: initial setup

---
+++ OpenTAKServer/opentakserver/blueprints/marti_api/citrap_api.py
@@ -0,0 +1,18 @@
+import bleach
+from flask import Blueprint
+from flask import current_app as app
+from flask import jsonify, request
+
+citrap_api_blueprint = Blueprint("citrap_api_blueprint", __name__)
+
+
+@citrap_api_blueprint.route("/Marti/api/missions/citrap/subscription", methods=["PUT"])
+def citrap_subscription():
+    uid = bleach.clean(request.args.get("uid"))
+    response = {"version": 3, "type": "com.bbn.marti.sync.model.MissionSubscription", "data": {}}
+    return "", 201
+
+
+@citrap_api_blueprint.route("/Marti/api/citrap")
+def citrap():
+    return jsonify([])
